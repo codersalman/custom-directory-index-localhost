3a180f4dbd4dcad3a1efe4af361c87e189a6a414
codersalman <coder88salman@gmail.com>
2022-05-19 00:45:25 +0530

    + search filter +additional param

--- index.php
+++ index.php
@@ -1,89 +1,92 @@
+<?php include "localhost.config"; ?>
+
 <html>
 <head>
-    <title>Custom Local Host</title>
+    <title><?php echo $localhost_title; ?></title>
     <link rel="shortcut icon" href=".favicon.ico">
     <link rel="stylesheet" href="style.css">
     <script src=sorttable.js"></script>
 </head>
 <div>
+    <h1 style="color: #FE4902">INDEX</h1>
 
-    <table >
-        <tr >
-            <th ><a href='./<?php echo $phpmyadminpath;?>'>
-            </a>PHP MyAdmin</th>
-            <th>Search : <input id="myInput" onkeyup="myFunction()" placeholder="Search for names.." type="text"  ></th>
-        </tr>
+    <table>
+        <tr>
+            <th style="width: 25%">Search : <input id="myInput" onkeyup="myFunction()" placeholder="Search for names.." type="text"></th>
+            <th style="width: 20% ; text-align: right;"><a style="color: #FFFFFF"" href="<?php echo $phpmyadmin_PATH;?>">PHP MyAdmin</a></th>
+
+             </tr>
+
 
     </table>
 
-<table id="myTable" class="sortable">
-    <thead>
+    <table id="myTable" class="sortable">
+        <thead>
 
+        <tr>
+            <th>Filename</th>
+            <th>Type</th>
+            <th>Size <small>(bytes)</small></th>
+            <th>Date Modified</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php
+        // Opens directory
+        $myDirectory=opendir(".");
 
-    <tr>
-        <th>Filename</th>
-        <th>Type</th>
-        <th>Size <small>(bytes)</small></th>
-        <th>Date Modified</th>
-    </tr>
-    </thead>
-    <tbody>
-    <?php
-    // Opens directory
-    $myDirectory=opendir(".");
+        // Gets each entry
+        while($entryName=readdir($myDirectory)) {
+            $dirArray[]=$entryName;
+        }
 
-    // Gets each entry
-    while($entryName=readdir($myDirectory)) {
-        $dirArray[]=$entryName;
-    }
+        // Finds extensions of files
+        function findexts($filename) {
+            $filename=strtolower($filename);
+            $exts=explode("[/\\.]", $filename);
+            $n=count($exts)-1;
+            $exts=$exts[$n];
+            return $exts;
+        }
 
-    // Finds extensions of files
-    function findexts($filename) {
-        $filename=strtolower($filename);
-        $exts=explode("[/\\.]", $filename);
-        $n=count($exts)-1;
-        $exts=$exts[$n];
-        return $exts;
-    }
+        // Closes directory
+        closedir($myDirectory);
 
-    // Closes directory
-    closedir($myDirectory);
+        // Counts elements in array
+        $indexCount=count($dirArray);
 
-    // Counts elements in array
-    $indexCount=count($dirArray);
+        // Sorts files
+        sort($dirArray);
 
-    // Sorts files
-    sort($dirArray);
+        // Loops through the array of files
+        for($index=0; $index < $indexCount; $index++) {
 
-    // Loops through the array of files
-    for($index=0; $index < $indexCount; $index++) {
+            // Gets File Names
+            $name=$dirArray[$index];
+            $namehref=$dirArray[$index];
 
-        // Gets File Names
-        $name=$dirArray[$index];
-        $namehref=$dirArray[$index];
+            // Gets Extensions
+            $extn=findexts($dirArray[$index]);
 
-        // Gets Extensions
-        $extn=findexts($dirArray[$index]);
+            // Gets file size
+            $size=number_format(filesize($dirArray[$index]));
 
-        // Gets file size
-        $size=number_format(filesize($dirArray[$index]));
+            // Gets Date Modified Data
+            $modtime=date("M j Y g:i A", filemtime($dirArray[$index]));
+            $timekey=date("YmdHis", filemtime($dirArray[$index]));
 
-        // Gets Date Modified Data
-        $modtime=date("M j Y g:i A", filemtime($dirArray[$index]));
-        $timekey=date("YmdHis", filemtime($dirArray[$index]));
-
-        // Print 'em
-        $class ='file';
-        print("
+            // Print 'em
+            $class ='file';
+            print("
       <tr class='$class'>
         <td><a href='./$namehref'>$name</a></td>
         <td><a href='./$namehref'>$extn</a></td>
         <td><a href='./$namehref'>$size</a></td>
         <td sorttable_customkey='$timekey'><a href='./$namehref'>$modtime</a></td>
       </tr>");
-    }
-    ?>
-    </tbody>
+        }
+        ?>
+        </tbody>
 </table>
 <h2><a href='./?hidden'>Show hidden files</a></h2>
 </div>
